a484252e913addee89a85f911afab998a8210ddb
Ahad-Chughtai <ahadchughtai007@gmail.com>
2023-12-14 19:49:45 -0600

    Added Scrollable Server List

--- Tron/src/client/ClientPanel.java
+++ Tron/src/client/ClientPanel.java
@@ -7,61 +7,66 @@ import server.BaseServer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientPanel extends JFrame {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 800;
     private BaseServer server;
     public static Client client;
-
+    private JList<String> serverList;
+    private JButton connectToServerButton;
     public ClientPanel() {
         super("Tron");
 
         setSize(WIDTH, HEIGHT);
 
-        //Title
+        // Title
         JLabel title = new JLabel("Tron");
-        title.setBounds(400, 10, 100, 50);
-        title.setForeground(Color.PINK);
+        title.setFont(new Font("Arial", Font.BOLD, 36));
+        title.setBounds(350, 50, 100, 50);
+        title.setForeground(Color.MAGENTA);
 
-        ImageIcon image = new ImageIcon("C:\\Users\\adico\\OneDrive\\Documents\\GitHub\\TronGame\\Tron\\maxresdefault.jpg"); // load the image to a imageIcon
-        Image scaledImage = image.getImage(); // transform it
-        Image newImg = scaledImage.getScaledInstance(800, 765,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
-        image = new ImageIcon(newImg);  // transform it back
-
-        //BG Label
-        JLabel bg = new JLabel();
-        bg.setIcon(image);
+        // Background Image
+        ImageIcon image = new ImageIcon("maxresdefault.jpg");
+        Image scaledImage = image.getImage().getScaledInstance(800, 765, Image.SCALE_SMOOTH);
+        image = new ImageIcon(scaledImage);
+        JLabel bg = new JLabel(image);
         bg.setBounds(0, 0, image.getIconWidth(), image.getIconHeight());
         bg.add(title);
 
-        //Create Server Button
-        JButton CreateServer = new JButton();
-        CreateServer.setText("make server");
-        CreateServer.setBounds(100, 100, 100, 100);
-        CreateServer.setVisible(true);
-        CreateServer.addActionListener(e -> createServer());
+        // Create Server Button
+        JButton createServer = createStyledButton("Make Server");
+        createServer.setBounds(50, 600, 200, 50);
+        createServer.addActionListener(e -> createServer());
 
-        //Connect Server Button
-        JButton ConnectToServer = new JButton();
-        ConnectToServer.setText("Connect to Server");
-        ConnectToServer.setBounds(200, 200, 100, 100);
-        ConnectToServer.setVisible(true);
-        ConnectToServer.addActionListener(e -> {
+        // Connect Server Button
+        connectToServerButton = createStyledButton("Connect to Server");
+        connectToServerButton.setBounds(300, 600, 200, 50);
+        connectToServerButton.setEnabled(false);
+
+        // Scrollable server list
+        DefaultListModel<String> serverListModel = new DefaultListModel<>();
+        serverList = new JList<>(serverListModel);
+        serverList.setBackground(Color.PINK);
+        serverList.setForeground(Color.WHITE);
+        JScrollPane serverScrollPane = new JScrollPane(serverList);
+        serverScrollPane.setBounds(550, 100, 200, 150);
+
+        serverList.addListSelectionListener(e -> connectToServerButton.setEnabled(!serverList.isSelectionEmpty()));
+        connectToServerButton.addActionListener(e -> {
             connectToServer();
             Base baseGame = new Base(client);
             add(baseGame);
-            CreateServer.setVisible(false);
-            ConnectToServer.setVisible(false);
+            createServer.setVisible(false);
+            connectToServerButton.setVisible(false);
+            serverScrollPane.setVisible(false);
 
-            JButton startGameButton = new JButton();
-            startGameButton.setText("start game");
-            startGameButton.setBounds(400, 400, 50, 50);
-            startGameButton.setVisible(true);
+            JButton startGameButton = createStyledButton("Start Game");
+            startGameButton.setBounds(550, 600, 200, 50);
             startGameButton.addActionListener(e1 -> {
-                remove(startGameButton);
+                startGameButton.setVisible(false);
                 StartGame startGame = new StartGame();
                 client.sendObject(startGame);
                 server.game.start();
@@ -71,31 +76,52 @@ public class ClientPanel extends JFrame {
             repaint();
         });
 
-        //add everything
+        // Add everything
         add(bg);
-        add(ConnectToServer);
-        add(CreateServer);
+        add(connectToServerButton);
+        add(createServer);
+        add(serverScrollPane);
+        serverScrollPane.setVisible(true);
+        connectToServerButton.setVisible(true);
 
         setLayout(null);
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    private JButton createStyledButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 16));
+        button.setForeground(Color.PINK);
+        button.setBackground(Color.MAGENTA);
+        button.setFocusPainted(false);
+        return button;
     }
 
     public static void main(String[] args) {
-        ClientPanel run = new ClientPanel();
+        SwingUtilities.invokeLater(() -> new ClientPanel());
     }
 
     public void createServer() {
         server = new BaseServer(6969);
         server.start();
-
+        ((DefaultListModel<String>) serverList.getModel()).addElement("Server at localhost:6969");
     }
 
     public void connectToServer() {
-        client = new Client("localhost", 6969);
-        client.connect();
+        int selectedIndex = serverList.getSelectedIndex();
+        if (selectedIndex != -1) {
+            String selectedServer = serverList.getModel().getElementAt(selectedIndex);
+            String[] parts = selectedServer.split(":");
+            int port = Integer.parseInt(parts[1].trim());
+            client = new Client("localhost", port);
+            client.connect();
 
-        JoinServer joinPacket = new JoinServer();
-        client.sendObject(joinPacket);
+            JoinServer joinPacket = new JoinServer();
+            client.sendObject(joinPacket);
+        }
     }
 }
+
